0f3de5f089355c457b0529b2a41bc05bdb428d6c
delphist <delphist@dataart.com>
2017-11-01 19:57:05 +0300

    Initial commit

---
+++ FlyoutMvvmFriendly.UWP.Example/FlyoutButtonBaseBehavior.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace FlyoutMvvmFriendly.UWP.Example
+{
+    public class FlyoutButtonBaseBehavior : Behavior<ButtonBase>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.Click += AssociatedObject_Click;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Click -= AssociatedObject_Click;
+
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var button = sender as ButtonBase;
+            if (button == null)
+                return;
+
+            DependencyObject parent = button;
+            while ((parent != null) && (!(parent is FlyoutPresenter)))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            var presenter = parent as FlyoutPresenter;
+            if (presenter == null)
+                return;
+
+            (presenter.Parent as Popup).IsOpen = false;
+        }
+
+    }
+}
---
+++ FlyoutMvvmFriendly.UWP.Example/MainPageViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlyoutMvvmFriendly.UWP.Example
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public string CurrentValue
+        {
+            get { return _currentValue; }
+            set { _currentValue = value; RaisePropertyChanged(); }
+        }
+        private string _currentValue = string.Empty;
+
+        public string NewValue
+        {
+            get { return _newValue; }
+            set { _newValue = value; RaisePropertyChanged(); }
+        }
+        private string _newValue = string.Empty;
+
+        public ICommand ApplyNewValue
+        {
+            get { return new CommandHandler(ApplyNewValueCommand); }
+        }
+        private void ApplyNewValueCommand()
+        {
+            CurrentValue = NewValue;
+        }
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName]string name = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+
+        #region ICommand dummy implementation, to be replaced with your MVVM framework command
+
+        private class CommandHandler : ICommand
+        {
+            private Action _action;
+            private bool _canExecute;
+            public CommandHandler(Action action, bool canExecute = true)
+            {
+                _action = action;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+                _action();
+            }
+        }
+
+        #endregion
+    }
+}
